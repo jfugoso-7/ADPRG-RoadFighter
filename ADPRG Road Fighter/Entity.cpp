9e18933182a72597861a661c6f9724b56fd47eba
jfugoso-7 <fugoso.john@gmail.com>
2025-04-04 23:01:57 +0800

    Fixed sf::Sprite errors in Entity.cpp and Entity.h

--- ADPRG Road Fighter/Entity.cpp
+++ ADPRG Road Fighter/Entity.cpp
@@ -1,24 +1,25 @@
 #include "Entity.h"
 #include "TextureManager.h"
 
-Entity::Entity() {};
-
-void Entity::setTexture(sf::Texture texture) {
-	mTexture = texture;
-	mSprite.setTexture(mTexture);
+Entity::Entity(const sf::Texture& texture)
+	: mTexture(&texture), mSprite(texture) // directly construct mSprite
+{
 }
 
-sf::Sprite* Entity::getSprite(){
+void Entity::setTexture(sf::Texture& texture) {
+	mTexture = &texture;
+	mSprite.setTexture(*mTexture);
+}
+
+sf::Sprite* Entity::getSprite() {
 	return &mSprite;
 }
 
 void Entity::update() {
 	if (clock.getElapsedTime().asMilliseconds() > 100) {
-		
-		sf::Texture* texture;
 		std::string key = rootTexName + std::to_string(ctr);
-		texture = TextureManager::getInstance()->getTexture(key);
-		setTexture(*texture);
+		sf::Texture* texture = TextureManager::getInstance()->getTexture(key);
+		setTexture(*texture); // now uses reference
 
 		ctr++;
 		ctr %= frames;
--- ADPRG Road Fighter/Entity.h
+++ ADPRG Road Fighter/Entity.h
@@ -1,18 +1,18 @@
 #include <SFML/Graphics.hpp>
 
-class Entity
-{
-	public:
-		Entity();
-		void setTexture(sf::Texture texture);
-		sf::Sprite* getSprite();
-		void update();
-		std::string rootTexName;
-		int frames;
-	private:
-		sf::Texture mTexture;
-		sf::Sprite mSprite;
-		sf::Clock clock;
-		int ctr = 0;
+class Entity {
+public:
+	Entity(const sf::Texture& texture);
+	void setTexture(sf::Texture& texture);
+	sf::Sprite* getSprite();
+	void update();
 
+	std::string rootTexName;
+	int frames;
+
+private:
+	const sf::Texture* mTexture = nullptr; // make it a const pointer
+	sf::Sprite mSprite;
+	sf::Clock clock;
+	int ctr = 0;
 };
